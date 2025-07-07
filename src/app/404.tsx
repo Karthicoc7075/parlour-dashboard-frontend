12f77aa2cc5c3a719572d804c4530d4c2a706715
Karthicoc7075 <karthicoc7075@gmail.com>
2025-07-07 18:51:42 +0530

    unused packages remove

---
+++ src/app/404.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Employee Not Found</h1>
+      <p>Sorry, the employee you are looking for does not exist.</p>
+      <a href="/dashboard/employees">Back to Employees List</a>
+    </div>
+  );
+}
--- src/app/dashboard/employees/[employeeId]/page.tsx
+++ src/app/dashboard/employees/[employeeId]/page.tsx
@@ -1,5 +1,5 @@
 import EmployeeDetailsClient from './clientComponent';
-
+import { notFound } from 'next/navigation';
 export type Attendance = {
   _id?: string;
   action: string;
@@ -26,24 +26,51 @@ export interface EmployeeId {
 
 
 interface EmployeePageProps {
-  params: { employeeId: string };
+  params: {  employeeId: string };
 }
 
-async function fetchAllEmployeeIds() {
+ async function fetchEmployeeById(employeeId:string) {
   try {
-    const response = await fetch('http://localhost:8888/api/v2/employee/get-ids', { cache: 'no-store' }); // Replace with your API
-    const employeesIds = await response.json();
-    return employeesIds
+    const response = await fetch(`http://localhost:8888/api/v2/employee/${employeeId}`, { cache: 'no-store' }); // Replace with your API
+    const employee = await response.json();
+    return employee;
   } catch (error) {
     console.error('Error fetching employee IDs:', error);
     return [];
   }
 }
+
+
+async function fetchAllEmployees() {
+  try {
+
+    const response = await fetch(`http://localhost:8888/api/v2/employee/get-ids`, { cache: 'no-store' }); // Replace with your API
+    
+    console.log('Response status:', response.status);
+    if(!response.ok) {
+      throw new Error('Failed to fetch employee IDs');
+    }
+   const employees = await response.json();
+    if (!Array.isArray(employees) || employees.length === 0) {
+      throw new Error('No employee IDs returned from API');
+    }
+    return employees;
+  } catch (error) {
+    console.error('Error fetching employees:', error);
+    return [];
+  }
+}
+
+
 export async function generateStaticParams() {
-  const employeeIds = await fetchAllEmployeeIds();
-  console.log('Employee IDs:', employeeIds);
-  return employeeIds.map((employee: EmployeeId) => ({
-    employeeId: employee._id,
+  const employees = await fetchAllEmployees();
+console.log('Fetched employee IDs:', employees);
+  if (!Array.isArray(employees) || employees.length === 0) {
+    notFound(); 
+  }
+
+  return employees.map((employee) => ({
+    employeeId: employee._id.toString(),
   }));
 }
 
@@ -51,7 +78,14 @@ export default async function EmployeeDetails({ params }: EmployeePageProps) {
   const employeeId = params.employeeId;
   console.log('Employee ID:', employeeId);
 
+ 
 
+
+const employee = await fetchEmployeeById(employeeId);
+
+  if (!employee) {
+    notFound(); 
+  }
   
 
   return (
