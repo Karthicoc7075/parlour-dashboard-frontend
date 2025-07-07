8388d9fa6c5d893c5405db1365ec4304d7e3e33a
Karthicoc7075 <karthicoc7075@gmail.com>
2025-07-07 18:53:31 +0530

    404 page fix

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 export default function NotFound() {
   return (
     <div>
       <h1>404 - Employee Not Found</h1>
       <p>Sorry, the employee you are looking for does not exist.</p>
-      <a href="/dashboard/employees">Back to Employees List</a>
+      <Link href="/dashboard/employees">Back to Employees List</Link>
     </div>
   );
 }
